8ffc07f4c8f6c07fe362a9c9f4a6d65f917910dc
Sonarays1234 <sonaliy10777@gmail.com>
2023-10-19 17:09:13 +0530

    variables&dataTypes

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12322
+let accountEmail = "sonaliy@gamil.com"
+var accountPassword = "1234"
+accountCity = "Indore"
+let accountState;
+
+//accountId = 12343243  //not allowed to change value of a const variable
+
+accountEmail = "sonaly123@gmail.com"
+accountPassword = "43232"
+accountCity = "Delhi"
+
+console.log(accountId);
+/*
+prefer not to use 'var'
+bcoz of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use script"; // Treate all JS code as newer version of JS
+
+
+//alert(3+3) //we r using nodejs,not browser
+console.log(3
+    +3);//don't do that...code readability shoube be high
+
+    let name  = "Sonali"
+    let age = 18
+    let isLoggedIn = false
+
+    //***************data types ******************/
+
+    //number => 2 tp power 53
+    //bigint => for largest num..used in stock market etc
+    //String => "abc"
+    // boolean => true/fales
+    // null => standalone value,representation of empty value
+    //undefined => value is not assigned yet
+    // symbol => for uniqueness
+    // object
+
+    console.log(typeof"sonali");
+    console.log(typeof age);
+    console.log(typeof null);   //it is an object type
+    console.log(typeof undefined);
